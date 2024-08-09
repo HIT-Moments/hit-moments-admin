ded8b7fc6f709ee15cae8e164ce13700175fb97a
Tung <ductungltt@gmail.com>
2024-08-09 15:38:19 +0700

    ✨ Add 404 - Error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,19 @@
+import { Button } from '@/components/ui/button';
+import ErrorImg from '../../assets/images/404error.png';
+import { useNavigate } from 'react-router';
+
 const ErrorPage = () => {
-  return <div className="text-4xl">404</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-10">
+      <img src={ErrorImg} alt="" className="w-[500px]" />
+      <p className="text-5xl font-bold">404 - PAGE NOT FOUND</p>
+      <Button className="rounded-full px-10 py-6 text-xl" onClick={() => navigate('/')}>
+        Back to home
+      </Button>
+    </div>
+  );
 };
 
 export default ErrorPage;
